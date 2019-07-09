e7e8b0d94e14cd51e9de7716afaa0a65502b403b
Konstantinos Christos Liosis <liosis@ceid.upatras.gr>
2019-07-09 04:12:06 +0300

    mongodb integration, first run

--- run.py
+++ run.py
@@ -4,16 +4,48 @@ An experimental app to check for possible ''patterns'' in OPAP's Virtual Footbal
 
 from flask import Flask
 import requests
-from bs4 import BeautifulSoup
+import json
+import pymongo
+import os
+import time
 
 
-# constants
 app = Flask(__name__)
-url = "https://virtualsports.opap.gr/virtual-results"
-match = "event-row not-printed-event res0 extra-results vs-table-row row"
-score = "col-md-1 col-sm-4 col-xs-4 hidden-sm hidden-xs vs-no-bg vs-home-score "
 
-page = requests.get(url)
-soup = BeautifulSoup(page.text, 'lxml')
-matches = soup.findAll("div", class_=match)
-print(matches)
+@app.route('/')
+def main_routine():
+    live = "false"
+    fromDate = "fromDate=" + "2019-07-07" #time.strftime("%Y-%d-%M")
+    toDate = "toDate=" + "2019-07-07" #time.strftime("%Y-%d-%M")
+
+    url = "https://api.opap.gr/sb/sport/virtual_soccer/coupon?locale=el&onlyLive="\
+            + live\
+            + "&marketIds=1%2C18&"\
+            + fromDate + '&'\
+            + toDate
+
+    response = json.loads(requests.get(url).text)
+
+    if "events" in response:
+        matches = response["events"]["content"]
+        entries = []
+        for item in matches:
+            entries.append({"id": item["id"], "time": item["dt"], "result": item["scr"]["EndResult"]})
+
+
+
+# # get mongoDB uri depending on the env
+# if os.environ["PATH"].startswith("/home/konstantinos/"):
+#     import configparser
+#     local_config = configparser.ConfigParser()
+#     local_config.read('data.ini')
+#     mongodb_uri = local_config["CREDS"]["mongodb"]
+# else:
+#     mongodb_uri = os.environ["MONGODB"]
+#
+# myclient = pymongo.MongoClient(mongodb_uri)
+# mydb = myclient["pliachicoDB"]
+# mycol = mydb["matches"]
+# x = mycol.insert_many(entries)
+
+    print(entries)
